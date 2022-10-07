3f8711e428a8c6c6392af2da796c40f379e97c26
Richard Felicilda <rfelicilda@dnamicro.com>
2022-10-07 15:00:41 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php
+  include 'dependencies.php'
+  ?>
+
+  <link rel="icon" href="./assets/images/logo.png" type="image/icon type">
+
+  <link rel="stylesheet" href="./assets/css/main.css">
+  <link rel="stylesheet" href="./assets/css/signin.css">
+
+  <title>SLSU-KTTO Document Management System</title>
+</head>
+
+<body>
+  <?php
+  include 'sign-in.php'
+  ?>
+</body>
+
+</html>
